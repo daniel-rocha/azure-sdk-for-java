1fbb400800cfdcb89d90d38c95bf5d4b22e9aac5
Katherine Hsu <weitzuhsu@microsoft.com>
2022-03-08 17:07:04 +0800

    add swagger test

---
+++ sdk/maps/azure-maps-traffic/swagger/src/main/java/TrafficCustomization.java
@@ -0,0 +1,9 @@
+import com.azure.autorest.customization.Customization;
+import com.azure.autorest.customization.LibraryCustomization;
+import com.azure.autorest.customization.PackageCustomization;
+import com.azure.autorest.customization.ClassCustomization;
+import com.azure.autorest.customization.MethodCustomization;
+import org.slf4j.Logger;
+
+public class TrafficCustomization extends Customization {
+}
